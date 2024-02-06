2c06c6caebe937dd7b9b88a51b36ae9cf9ca3ff3
Fretoss <157448320+Fretoss@users.noreply.github.com>
2024-02-06 11:19:55 +0500

    Update prak7_1.kt

--- prak7_1.kt
+++ prak7_1.kt
@@ -12,9 +12,9 @@ fun main() {
                     n == 1 -> println("Значение первого члена прогрессии: $first")
                     n == 5 -> println("Значение пятого члена прогрессии: $fifth")
                     n > 5 -> {
-                        val d = (fifth - first) / 4
-                        val nth = first + (n - 1) * d
-                        val sum = (n / 2) * (first + nth)
+                        var d = (fifth - first) / 4
+                        var nth = first + (n - 1) * d
+                        var sum = (n / 2) * (first + nth)
                         println("Значение $n-го члена прогрессии: $nth")
                         println("Сумма $n членов: $sum")
                     }else -> println("Член прогрессии с номером $n не существует")
